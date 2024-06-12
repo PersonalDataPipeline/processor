972588e912a85386b341195db6c2be31e4b97657
Josh Cunningham <josh@joshcanhelp.com>
2024-06-12 09:50:32 -0700

    feat: add recipe validator

---
+++ src/utils/validate-recipe.ts
@@ -0,0 +1,112 @@
+import Joi from "joi";
+import { strict as assert } from "node:assert";
+
+import getConfig, { Config } from "../utils/config.js";
+import path from "path";
+import { readdirSync } from "fs";
+import { OutputHandler } from "./types.js";
+
+interface InputObject {
+  [key: string]: {
+    [key: string]: string;
+  };
+}
+
+interface OutputObject {
+  [key: string]: {
+    strategy: string;
+    template: string;
+    data?: object;
+  }[];
+}
+
+interface RecipeObject {
+  input: InputObject;
+  output: OutputObject;
+  pipeline: object[];
+}
+
+export const validateRecipe = async (recipe: object, config: Config) => {
+  const { value, error } = Joi.object({
+    input: Joi.object().pattern(
+      Joi.string().valid(...getConfig().inputsSupported),
+      Joi.object()
+        .pattern(
+          Joi.string(),
+          Joi.object().pattern(Joi.string(), Joi.string().pattern(/^[a-z\d_]*$/))
+        )
+        .unknown()
+    ),
+    output: Joi.object().pattern(
+      Joi.string().valid(...getConfig().outputsSupported),
+      Joi.array().items(
+        Joi.object({
+          strategy: Joi.string().required(),
+          data: Joi.object().unknown(),
+        })
+      )
+    ),
+    pipeline: Joi.array().items(
+      Joi.object({
+        // TODO: Validate that this exists as an input field
+        field: Joi.string().required(),
+        // TODO: Validate that these exist as built-in or plugin
+        transform: Joi.array().items(Joi.string()),
+        // TODO: Validate that this exists as an input field
+        linkTo: Joi.string(),
+      })
+    ),
+  }).validate(recipe);
+
+  if (error) {
+    throw new Error(`Recipe validation: ${error.details[0].message}`);
+  }
+
+  const inputNames = Object.keys((value as RecipeObject).input);
+  for (const inputName of inputNames) {
+    for (const subName of Object.keys((value as RecipeObject).input[inputName])) {
+      const dataPath = path.join(config.inputsDir, inputName, subName);
+      try {
+        // TODO: No data should not always throw
+        const inputData = readdirSync(dataPath);
+        assert(inputData.length >= 1);
+      } catch (error) {
+        throw new Error(`No data found for input ${inputName}.${subName}`);
+      }
+    }
+  }
+
+  const outputNames = Object.keys((value as RecipeObject).output);
+  for (const outputName of outputNames) {
+    const { default: outputHandler } = (await import(
+      `../outputs/${outputName}/index.js`
+    )) as {
+      default: OutputHandler;
+    };
+
+    if (!outputHandler.isReady()) {
+      throw new Error(`Output handler ${outputName} is not configured properly.`);
+    }
+
+    for (const output of (value as RecipeObject).output[outputName]) {
+      const { strategy: strategyName, data: strategyData } = output;
+
+      const outputStrategy = outputHandler.handlers
+        .filter((strategy) => strategy.name() === strategyName)
+        .pop();
+
+      if (typeof outputStrategy === "undefined") {
+        throw new Error(`Invalid output strategy: ${strategyName}`);
+      }
+
+      const strategyErrors = outputStrategy.isReady(strategyData);
+      if (strategyErrors.length > 0) {
+        throw new Error(
+          `Output strategy ${strategyName} is not configured: \n${strategyErrors.join("\n")}`
+        );
+      }
+    }
+  }
+
+  return value;
+};
